a3c8814c4023acda8baa3cd1d778b1bc28fd7ee3
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-07-10 02:52:49 +0500

    Make header dynamic

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { getCategoriesApi, getCurrenciesApi } from 'api/header'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import 'scss/header/header.css'
+import useApi from 'utils/hooks/useApi'
 
 const Header = () => {
+    const [, { data }] = useApi(getCategoriesApi)
+    const [, { data: currencies }] = useApi(getCurrenciesApi)
+
     const [show, setShow] = useState(true)
     const [clshow, setclShow] = useState(false)
+    const [activeItem, setActiveItem] = useState([])
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -37,9 +43,11 @@ const Header = () => {
                             </div>
                             <div className='col-sm-3'>
                                 <div className='toph-right'>
-                                    <div className='sel-currancy'>
+                                    <div className='sel-currancy' >
                                         <select>
-                                            <option>Select Currency</option>
+                                            {currencies?.map(currency => (
+                                                <option style={{ color: 'black' }} key={currency.id} value={currency.id}>{currency.currency_code}</option>
+                                            ))}
                                         </select>
                                     </div>
                                     <div className='sel-language'>
@@ -108,87 +116,28 @@ const Header = () => {
                                 </div>
                                 {
                                     show ? <ul className='list-group'>
-                                        <li className='list-group-item'><a href="/"><img src="/images/cellphones.svg" alt="cellphones" /><span>Cell Phones</span></a></li>
-                                        <li className='list-group-item active'><a onMouseEnter={() => setclShow(true)} onMouseLeave={() => setclShow(false)} href="/"><img src="/images/computer-acc.svg" alt="computeracc" /><span>Computers &amp; Accessories</span> <i><img src="/images/arrow.svg" alt="arrow" /></i></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/tv.svg" alt="Television" /><span>Television &amp; Video</span></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/shoes.svg" alt="shoes" /><span>Shoes</span> <i><img src="/images/arrow.svg" alt="arrow" /></i></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/clothes.svg" alt="clothes" /><span>Clothes</span> <i><img src="/images/arrow.svg" alt="arrow" /></i></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/cellphones.svg" alt="cellphones" /><span>Cell Phones</span> <i><img src="/images/arrow.svg" alt="arrow" /></i></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/computer-acc.svg" alt="computeracc" /><span>Computers &amp; Accessories</span></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/tv.svg" alt="Television" /><span>Television &amp; Video</span></a></li>
-                                        <li className='list-group-item'><a href="/"><img src="/images/shoes.svg" alt="shoes" /><span>Shoes</span></a><i className='save'>Save 25%</i></li>
-                                        {/* <li className='list-group-item'><a href="/"><img src="http://127.0.0.1:5500/src/new-pages/assets/images/clothes.svg" alt="clothes" /><span>Clothes</span></a></li> */}
+                                        {data?.map((item: any) => (
+                                            item?.child_categories.length > 0 ? <li key={item.id} className='list-group-item active'><a onMouseEnter={() => { setclShow(true); setActiveItem(item.child_categories) }} onMouseLeave={() => { setclShow(false); }} href="/"><img src={item?.icon} alt="computeracc" /><span>{item.name}</span> <i><img src="/images/arrow.svg" alt="arrow" /></i></a></li> :
+                                                <li key={item.id} className='list-group-item'><a href="/"><img src={item.icon} alt="cellphones" /><span>{item.name}</span></a></li>
+                                        ))}
+
                                     </ul> : null
                                 }
                                 {
-                                    clshow ? <div className='category-list' onMouseEnter={() => setclShow(true)} onMouseLeave={() => setclShow(false)}>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                        <div className='category-item'>
-                                            <h3>Category Item Name</h3>
-                                            <ul className=''>
-                                                <li className=''><a href="/"><span>Shoes</span></a></li>
-                                                <li className=''><a href="/"><span>Clothes</span></a></li>
-                                                <li className=''><a href="/"><span>Cell Phones</span></a></li>
-                                                <li className=''><a href="/"><span>Lorem ipsum</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                                <li className=''><a href="/"><span>Auto Electronic</span></a></li>
-                                            </ul>
-                                        </div>
-                                    </div> : null
+                                    clshow && <div className='category-list' onMouseEnter={() => setclShow(true)} onMouseLeave={() => setclShow(false)}>
+                                        {activeItem?.map((item: any) => (
+                                            <div key={item.id} className='category-item'>
+                                                <h3>{item?.name}</h3>
+                                                <ul className=''>
+                                                    {item?.child_categories.map((childItem: any) => (
+                                                        <li key={childItem.key} className=''><a href="/"><span>{childItem.name}</span></a></li>
+
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        ))}
+
+                                    </div>
                                 }
                             </div>
                             <div className='col-sm-9'>
